b0a078e40b830640f61449ee333220e39e9e552f
Nonclaing <32881294+Nonclaing@users.noreply.github.com>
2022-01-27 19:21:22 +0300

    Слайдер, правки
    -В хедер добавлены отступы у номеров и br в текст
    -Изменён фон у карточек, теперь он слева от экрана
    -Добавлено wrap карточкам
    -Добавлен слайдер(без стилей боковых карточек)
    -Добавлено пару отступов в footer
    -Не много переместил правый объект декора в footer-е

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: true,
+    speed: 400,
+    spaceBetween: 70,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    toggle: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        800: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    },
+
+});
